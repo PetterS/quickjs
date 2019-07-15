cf165b8d9f510ee7035925535cd6137b4aa1767b
Petter Strandmark <petter.strandmark@gmail.com>
2019-07-15 23:13:12 +0200

    Try to fix the missing headers problem with setuptools.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 import glob
+import sys
+
 from setuptools import setup, Extension
+
+def get_c_sources(include_headers=False):
+      sources=['module.c'] + glob.glob("third-party/*.c")
+      if include_headers:
+            sources += glob.glob("third-party/*.h")
+      return sources
 
 _quickjs = Extension('_quickjs',
                      define_macros=[('CONFIG_VERSION', '"2019-07-09"')],
-                     sources=['module.c'] + glob.glob("third-party/*.c"),
+                     # HACK.
+                     # See https://github.com/pypa/packaging-problems/issues/84.
+                     sources=get_c_sources(include_headers=("sdist" in sys.argv)),
                      headers=glob.glob("third-party/*.h"))
 
 long_description = """
@@ -14,7 +24,7 @@ setup(author="Petter Strandmark",
       author_email="petter.strandmark@gmail.com",
       name='quickjs',
       url='https://github.com/PetterS/quickjs',
-      version='1.1.0',
+      version='1.1.1',
       description='Wrapping the quickjs C library.',
       long_description=long_description,
       packages=["quickjs"],
