91745fa1294439d5293c2e8a2657e732573ff0ee
Petter Strandmark <petter.strandmark@gmail.com>
2019-08-11 18:10:12 +0200

    Disallow multiple threads using the same Context, even at different times (#4)
    * Disallow multiple threads using the same Context, even at different times.
    
    * Bump version.

--- quickjs/__init__.py
+++ quickjs/__init__.py
@@ -1,5 +1,7 @@
+import concurrent.futures
 import json
 import threading
+from typing import Tuple
 
 import _quickjs
 
@@ -15,15 +17,33 @@ StackOverflow = _quickjs.StackOverflow
 
 
 class Function:
-    def __init__(self, name: str, code: str) -> None:
-        self._context = Context()
-        self._context.eval(code)
-        self._f = self._context.get(name)
+    # There are unit tests demonstrating that we are crashing if different threads are accessing the
+    # same runtime, even if it is not at the same time. So we run everything on the same thread in
+    # order to prevent this.
+    _threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+    
+    def __init__(self, name: str, code: str, *, own_executor=False) -> None:
+        """
+        Arguments:
+            name: The name of the function in the provided code that will be executed.
+            code: The source code of the function and possibly helper functions, classes, global
+                  variables etc.
+            own_executor: Create an executor specifically for this function. The default is False in
+                          order to save system resources if a large number of functions are created.
+        """
+        if own_executor:
+            self._threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=1)
         self._lock = threading.Lock()
+
+        future = self._threadpool.submit(self._compile, name, code)
+        concurrent.futures.wait([future])
+        self._context, self._f = future.result()
 
     def __call__(self, *args, run_gc=True):
         with self._lock:
-            return self._call(*args, run_gc=run_gc)
+            future = self._threadpool.submit(self._call, *args, run_gc=run_gc)
+            concurrent.futures.wait([future])
+            return future.result()
 
     def set_memory_limit(self, limit):
         with self._lock:
@@ -48,6 +68,12 @@ class Function:
         """
         with self._lock:
             self._context.gc()
+
+    def _compile(self, name: str, code: str) -> Tuple[Context, Object]:
+        context = Context()
+        context.eval(code)
+        f = context.get(name)
+        return context, f
 
     def _call(self, *args, run_gc=True):
         def convert_arg(arg):
