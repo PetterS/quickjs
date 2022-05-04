586cbfc6be6a4d2d01049b4d26792d53e2a641da
matpi <matpi@protonmail.ch>
2022-05-04 16:55:08 +0200

    Remove prefix stuff from arch; test make instead of a shell script.

--- ci_build.sh
+++ ci_build.sh
@@ -1,9 +1,2 @@
-if [ $IS_64 == 1 ]
-then
-	PREFIX=""
-else
-	PREFIX="linux32"
-fi
-
-$PREFIX python$PYTHON_VERSION setup.py build_ext --inplace
-$PREFIX python$PYTHON_VERSION -X dev -m unittest
+python$PYTHON_VERSION setup.py build_ext --inplace
+python$PYTHON_VERSION -X dev -m unittest
